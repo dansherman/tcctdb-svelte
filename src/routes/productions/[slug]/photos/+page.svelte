ecd0ccc159444c987902600ad7f5d6ded3da9d63
dansherman <dan@dksherman.com>
2023-04-17 08:47:28 -0500

    better photo handling

--- src/routes/productions/[slug]/photos/+page.svelte
+++ src/routes/productions/[slug]/photos/+page.svelte
@@ -1,42 +1,52 @@
 <script lang="ts">
-  import type {Photo} from "$lib/types"
-import { urlFor } from "$lib/img-url.js";
-import Masonry from 'svelte-bricks'
-import { selectedImage, modalOpen } from '$lib/stores';
-export let data;
+  import { goto } from '$app/navigation';
+  import type { Photo } from "$lib/types";
+  import { urlFor } from "$lib/img-url.js";
+  import Masonry from "svelte-bricks";
+  import { selectedImage, modalOpen } from "$lib/stores";
+  export let data;
   let { production } = data;
-let items:Array<Photo> = production.productionPhotos
+  let items: Array<Photo> = production.productionPhotos;
   if (items == null) {
-    items = []
+    items = [];
   }
-	let i = 0
-	for (let item of items) {
-		item.id = i
-		i++
-	}
-	let minColWidth = 192;
-	let maxColWidth = 256;
-	let gap = 4;
-	$selectedImage = items[0]
-	let h:number
-let w:number
-
+  let i = 0;
+  for (let item of items) {
+    item.id = i;
+    i++;
+  }
+  let minColWidth = 192;
+  let maxColWidth = 256;
+  let gap = 4;
+  $selectedImage = items[0];
+  let h: number;
+  let w: number;
+  const handlePhotoClick = (item) => {
+    console.log(item)
+    $selectedImage = item;
+    //if mobile then download/open otherwise modal
+    if (w > 450) {
+      $modalOpen = true;
+    } else {
+      goto(urlFor(item.photo).url())
+    }
+  };
 </script>
-<svelte:window bind:innerHeight={h} bind:innerWidth={w}/>
+
+<svelte:window bind:innerHeight={h} bind:innerWidth={w} />
 <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-1">
-{#each items as item}
-
-			
-		
-			
-  {#if w > 450}
-  <button on:click={()=>{$selectedImage = item; $modalOpen = true}}><img class="mx-auto w-96" src={urlFor(item.photo)?.width(384).url()} alt={item.caption} /></button>
+  {#each items as item}
+    <button
+      on:click={() => {
+        handlePhotoClick(item);
+      }}
+      ><img
+        class="mx-auto w-96"
+        src={urlFor(item.photo)?.width(384).url()}
+        alt={item.caption}
+      /></button
+    >
   {:else}
-  <img class="mx-auto w-96" src={urlFor(item.photo)?.width(384).url()} alt={item.caption} />
-  {/if}
-
-
-{:else}
-<div class="">No photos found... ☹️</div>
-{/each}
+    <div class="">No photos found... ☹️</div>
+  {/each}
 </div>
