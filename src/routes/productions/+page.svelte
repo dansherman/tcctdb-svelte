b0d8c20511720191b0ba70a59694faeb9ef532db
dansherman <dan@dksherman.com>
2023-08-14 14:28:40 -0500

    Fix productions page

--- src/routes/productions/+page.svelte
+++ src/routes/productions/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	export let data;
-	let productions = data
+	let {productions} = data
   import Title from '$components/Title.svelte';
 </script>
 
@@ -10,7 +10,7 @@
 <Title>Productions</Title>
 <div class="sm:flex flex-wrap justify-between pt-6">
   {#each productions as production}
-  <a href="/productions/{production.slug.current.current}">  
+  <a href="/productions/{production.slug.current}">  
     <div class="mx-auto border-indigo-500 bg-indigo-100 rounded-md border-2 inset-2 h-72 w-48 mb-3.5 flex flex-col justify-between shadow-md">
       <div class="mx-auto text-center px-4 pt-2">
         <p class="text-lg font-extralight">{production.company.name}</p>
--- src/routes/productions/+page.ts
+++ src/routes/productions/+page.ts
@@ -1,7 +1,21 @@
-import { supabase } from "$lib/supabase";
-export const load = async () => {
-  const { data: productions } = await supabase
-    .from('productions')
-    .select(`show(title), poster, company(name), slug.current`)
-  return { productions }
+import client from "$lib/sanityClient";
+export const prerender = false;
+export async function load() {
+  const query = `*[_type == 'production']{
+    slug,
+    show->{title},
+    poster,
+    company->{name, slug},
+    'year':performanceDates[0].dateAndTime,
+  }`;
+  let productions = await client.fetch(query);
+  productions = productions.map((p) => {
+    if (p.year != null) {
+      p.year = p.year.slice(0, 4);
+    } else {
+      p.year = "—";
+    }
+    return p;
+  });
+  return { productions };
 }
